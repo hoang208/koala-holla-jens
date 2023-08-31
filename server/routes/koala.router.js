19557d47f4f6c9cfa38d5f3ffc692bf103d9df4b
Najma Jama <Najma55.nj@gmail.com>
2023-08-31 14:48:37 -0500

    added put info

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,12 +5,50 @@ const koalaRouter = express.Router();
 
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas_info" ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting koalas', error);
+        res.sendStatus(500);
+    });
+});
 
 // POST
 
 
 // PUT
+koalaRouter.put('/koala_Pool/:id', (req, res) => {
+   
+
+    let idToUpdate = req.params.id;
+    let newKoala = req.body.artist;
+
+    console.log('idToUpdate:', idToUpdate);
+    console.log(':', );
+
+    console.log('typeof newKoala:', typeof newKoala);
+  
+    let mySqlQuery = `
+    UPDATE "" SET "" = $1 WHERE id = $2;
+    `;
+
+    pool.query(mySqlQuery, [, idToUpdate])
+    
+        .then(
+            (response) => {
+                console.log("Update request successful", idToUpdate);
+                res.sendStatus(200);
+            }
+        )
+        .catch(
+            (err) => {
+                console.log(`Update request failed: ${idToUpdate}`, err);
+                res.sendStatus(500);
+            }
+        )
+})
 
 
 // DELETE
